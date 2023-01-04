eae737cab905920f443af058ef5381d3bc3e6f6b
Sathira <sathira.williams@gmail.com>
2023-01-04 17:14:39 -0500

    Initial ThreeJS draw of Lorenz Attractor

--- static/main.js
+++ static/main.js
@@ -11,25 +11,57 @@ var beta = 8.0/3.0;
 var x = 0.01;
 var y = 0;
 var z = 0;
-var scale = 15;
+var dt = 0.01;
 
 const size = 10;
 const divisions = 10;
 
-const gridHelper = new THREE.GridHelper( size, divisions );
-scene.add( gridHelper );
+const geometry = new THREE.SphereGeometry();
 
-const geometry = new THREE.SphereGeometry( 0.5, 32, 16 );
-const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-const sphere = new THREE.Mesh( geometry, material );
-// sphere.position.set(0,0,10)
-// sphere.position.copy();
-scene.add( sphere );
+for (let i = 0; i < 1000; i++){
+  const dxdt = sigma * (y - x) * dt;
+  const dydt = (x * (rho - z) - y) * dt;
+  const dzdt = (x * y - beta * z) * dt;
+	x += dxdt;
+	y += dydt;
+	z += dzdt;
+	// const geometry = new THREE.SphereGeometry( 0.1, 32, 16 );
+	// geometry.vertices.push(new THREE.Vector3(x,y,z));
+	// const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+	// const sphere = new THREE.Mesh( geometry, material );
+	geometry.vertices.push(new THREE.Vector3(x, y, z));
+	// scene.add( sphere );
+}
 
+const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+const line = new THREE.Line(geometry, material);
+scene.add(line);
+
+// const geometry = new THREE.SphereGeometry( 0.1, 32, 16 );
+// const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+// const sphere = new THREE.Mesh( geometry, material );
+// sphere.position.set(0,0,10) s
+// sphere.position.copy(); s
+// function createSphere() {
+// 	dxdt = sigma*(y-x);
+// 	dydt = (rho * x) - y - (x*z);
+// 	dzdt = x * y - beta * z;
+// 	x = dxdt;
+// 	y = dydt;
+// 	z = dzdt;
+// 	const geometry = new THREE.SphereGeometry( 0.1, 32, 16 );
+// 	geometry.vertices.push(new THREE.Vector3(x,y,z));
+// 	const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+// 	const sphere = new THREE.Mesh( geometry, material );
+// 	return sphere
+// }
+
+// scene.add( sphere );
 var animate = function () {
 	requestAnimationFrame( animate );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	renderer.render( scene, camera );
+	renderer.setClearColor( 0xffffff );
 };
 
 animate();
